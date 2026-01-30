5e64f0c45971f984fe1705d0cf57703afa8fccb1
karlmage <karlmagegit@gmail.com>
2026-01-30 19:21:21 +0100

    Starts off algorythm study

---
+++ algorythms/sort_algorythms/bubble_sort/bubble_sort.py
@@ -0,0 +1,21 @@
+def bubble_sort(array):
+    n = len(array)
+
+    for i in range(n):
+        already_sorted = True
+
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+                already_sorted = False
+
+        if already_sorted:
+            break
+
+    return array
+
+if __name__ == "__main__":
+    from algorythms.sort_algorythms.algorythm_timing import run_sorting_algorythm
+
+    run_sorting_algorythm("bubble_sort")

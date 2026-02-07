6be107ba0499bd01cad650fb3d929dee4bd457b7
karlmage <karlmagegit@gmail.com>
2026-02-07 10:10:55 +0100

    Add merge sort
    # Conflicts:
    #	algorythms/sort_algorythms/merge_sort/merge_sort.py

---
+++ algorythms/sort_algorythms/merge_sort/merge_sort.py
@@ -0,0 +1,41 @@
+# Source: https://realpython.com/sorting-algorithms-python/
+
+def merge(left, right):
+    if len(left) == 0:
+        return right
+    if len(right) == 0:
+        return left
+
+    result = []
+    index_left = index_right = 0
+
+    while len(result) < len(left) + len(right):
+        if left[index_left] < right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+def merge_sort(array):
+
+    if len(array) < 2:
+        return array
+
+    midpoint = len(array) // 2
+
+    return merge(merge_sort(array[:midpoint]),
+                 merge_sort(array[midpoint:]))
+
+if __name__ == "__main__":
+    from algorythms.sort_algorythms.algorythm_timing import run_sorting_algorythm
+    run_sorting_algorythm("merge_sort")
+
